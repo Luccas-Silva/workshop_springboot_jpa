96b8ca7485ff13054bc528498f15ca17e5f364c6
Luccas Silva <lluccas.anjoss@gmail.com>
2022-11-02 22:29:07 -0300

    first commit

---
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/CourseStoicoApplication.java
@@ -0,0 +1,13 @@
+package com.Stoico.course_Stoico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseStoicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseStoicoApplication.class, args);
+	}
+
+}
---
+++ course_Stoico/src/test/java/com/Stoico/course_Stoico/CourseStoicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stoico.course_Stoico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseStoicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
