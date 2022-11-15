17c2bac9eed6fc6b0e685b04092a048d4ffbb349
Luccas Silva <lluccas.anjoss@gmail.com>
2022-11-15 10:01:50 -0300

    Service layer, component registration

---
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/services/UserService.java
@@ -0,0 +1,26 @@
+package com.Stoico.course_Stoico.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Stoico.course_Stoico.entities.User;
+import com.Stoico.course_Stoico.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
