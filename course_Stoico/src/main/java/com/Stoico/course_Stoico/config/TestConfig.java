b1c103b477403d92c76777eb01d353efe675880c
Luccas Silva <lluccas.anjoss@gmail.com>
2023-01-02 20:36:16 -0300

    Category

--- course_Stoico/src/main/java/com/Stoico/course_Stoico/config/TestConfig.java
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.Stoico.course_Stoico.entities.Category;
 import com.Stoico.course_Stoico.entities.Order;
 import com.Stoico.course_Stoico.entities.User;
 import com.Stoico.course_Stoico.entities.enums.OrderStatus;
+import com.Stoico.course_Stoico.repositories.CategoryRepository;
 import com.Stoico.course_Stoico.repositories.OrderRepository;
 import com.Stoico.course_Stoico.repositories.UserRepository;
 
@@ -24,8 +26,18 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Computers"); 
+		Category cat3 = new Category(null, "Books");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Stoico", "stoico@gmail.com", "9999999", "1234567");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -34,6 +46,7 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2022-10-21T03:42:10Z"), OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.parse("2022-11-22T15:21:22Z"), OrderStatus.SHIPPED, u3);
 		Order o4 = new Order(null, Instant.parse("2022-11-24T06:45:18Z"), OrderStatus.DELIVERED, u1);
+		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4));
---
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.Stoico.course_Stoico.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Stoico.course_Stoico.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
