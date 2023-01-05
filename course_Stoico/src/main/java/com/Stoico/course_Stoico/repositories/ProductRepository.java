d773794de58ec7ec01adc620b07dda7067c20908
Luccas Silva <lluccas.anjoss@gmail.com>
2023-01-04 22:04:02 -0300

    Product

---
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.Stoico.course_Stoico.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Stoico.course_Stoico.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	
+
+}
---
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.Stoico.course_Stoico.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Stoico.course_Stoico.entities.Product;
+import com.Stoico.course_Stoico.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
