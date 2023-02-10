b430d60c9decf712aa17d016de46f8241c68dea5
Luccas Silva <lluccas.anjoss@gmail.com>
2023-02-09 23:52:05 -0300

    Exception handling - findById

--- course_Stoico/src/main/java/com/Stoico/course_Stoico/services/UserService.java
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.Stoico.course_Stoico.entities.User;
 import com.Stoico.course_Stoico.repositories.UserRepository;
+import com.Stoico.course_Stoico.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
