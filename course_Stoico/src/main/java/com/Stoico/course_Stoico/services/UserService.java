d089af7c8fbef253bf527fba965f3775dc5067b4
Luccas Silva <lluccas.anjoss@gmail.com>
2023-02-25 09:18:07 -0300

    Exception handling - update

--- course_Stoico/src/main/java/com/Stoico/course_Stoico/services/UserService.java
+++ course_Stoico/src/main/java/com/Stoico/course_Stoico/services/UserService.java
@@ -13,6 +13,8 @@ import com.Stoico.course_Stoico.repositories.UserRepository;
 import com.Stoico.course_Stoico.services.exceptions.DatabaseException;
 import com.Stoico.course_Stoico.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);	
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
